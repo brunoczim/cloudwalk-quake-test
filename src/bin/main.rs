f76b06b6a397d8c184653522ea4835058d618e25
brunoczim <brunoczim@gmail.com>
2023-08-28 17:26:53 -0300

    setup logger in the main

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,19 +1,41 @@
 use clap::Parser as CliParser;
 use quake_log_parser::{error::Result, parser::Parser, report::LogReport};
-use std::{fs::File, io, path::PathBuf, process::exit};
+use simplelog::{Config, WriteLogger};
+use std::{
+    fs::{File, OpenOptions},
+    io,
+    path::PathBuf,
+    process::exit,
+};
 
 #[derive(Debug, Clone, CliParser)]
 struct Arguments {
     #[arg(default_value = "qgames.log")]
     input_path: PathBuf,
+    #[arg(short = 'l', long = "app-log", default_value = "script.log")]
+    log_path: PathBuf,
 }
 
 fn try_main() -> Result<()> {
     let args = Arguments::parse();
-    let file = File::open(&args.input_path)?;
-    let parser = Parser::new(file);
+
+    let script_log_file = OpenOptions::new()
+        .read(false)
+        .write(true)
+        .append(true)
+        .create(true)
+        .open(&args.log_path)?;
+    WriteLogger::init(
+        log::LevelFilter::Warn,
+        Config::default(),
+        script_log_file,
+    )?;
+
+    let quake_file = File::open(&args.input_path)?;
+    let parser = Parser::new(quake_file);
     let report = LogReport::generate(parser)?;
     serde_json::to_writer_pretty(io::stdout(), &report)?;
+
     Ok(())
 }
 
